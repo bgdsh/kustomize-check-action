934d9bbac798fe18d37b9cf772b22fe5f964ed8a
Guodong Bai <bgd_sh@hotmail.com>
2022-06-17 18:08:17 +0800

    feat: 🎸 install kustomize

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 const core = require('@actions/core');
 const github = require('@actions/github');
+const {execFileSync} = require('child_process')
 
 try {
   // `files` input defined in action metadata file
@@ -11,6 +12,8 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  const result = execFileSync('./install_kustomize.sh');
+  console.log(`install kustomize result: ${result.toString('utf-8')}`)
 } catch (error) {
   core.setFailed(error.message);
 }
---
+++ install_kustomize.sh
@@ -0,0 +1,28 @@
+echo "getting download url for kustomize ${kustomize_version}"
+for i in {1..100}; do
+    url=$(curl -s "https://api.github.com/repos/kubernetes-sigs/kustomize/releases?per_page=100&page=$i" | jq -r '.[].assets[] | select(.browser_download_url | test("kustomize(_|.)?(v)?'$kustomize_version'_linux_amd64"))  | .browser_download_url')
+    if [ ! -z $url ]; then 
+        echo "Download URL found in $url"
+        break
+    fi
+done
+
+echo "Downloading kustomize v${kustomize_version}"
+if [[ "${url}" =~ .tar.gz$ ]]; then
+    curl -s -S -L ${url} | tar -xz -C /usr/bin
+else
+    curl -s -S -L ${url} -o /usr/bin/kustomize
+fi
+if [ "${?}" -ne 0 ]; then
+    echo "Failed to download kustomize v${kustomize_version}."
+    exit 1
+fi
+echo "Successfully downloaded kustomize v${kustomize_version}."
+
+echo "Allowing execute privilege to kustomize."
+chmod +x /usr/bin/kustomize
+if [ "${?}" -ne 0 ]; then
+    echo "Failed to update kustomize privilege."
+    exit 1
+fi
+echo "Successfully added execute privilege to kustomize."
